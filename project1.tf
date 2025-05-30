f1ef25cb0673011ace6a7079bf6e2cb31853736c
Kyle Benson <kyleabenson@gmail.com>
2025-05-29 17:35:53 -0700

    updated TF to add otel

--- project1.tf
+++ project1.tf
@@ -10,39 +10,60 @@ resource "google_cloud_run_v2_service" "default" {
       ports {
         container_port = 8000
       }
-      image = "ghcr.io/kyleabenson/apprepo:main"
+      image = " us-west1-docker.pkg.dev/kb-workspace/apprun-repo/apprun:latest"
       depends_on = ["collector"]
-    #   env {
-    #     name = "OTEL_SERVICE_NAME"
-    #     value = "myapp"
-    #     name ="OTEL_TRACES_EXPORTER"
-    #     value = "otlp"
-    #     name = "OTEL_EXPORTER_OTLP_ENDPOINT"
-    #     value = "http://localhost:4317"
-    #   }
+      env {
+        name = "OTEL_SERVICE_NAME"
+        value = "backend"
+      }
+      env {
+        name ="OTEL_TRACES_EXPORTER"
+        value = "otlp"
+      }
+      env {
+        name = "OTEL_EXPORTER_OTLP_ENDPOINT"
+        value = "http://localhost:4317"
+      }
     }
-    # containers {
-    #   name = "collector"
-    #   image = "us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.126.0"
-    #   args = "--config=/etc/otelcol-google/config.yaml"
-    #   startup_probe {
-    #     http_get {
-    #       port = 13133
-    #       path = "/"
-    #     timeoutSeconds = 30
-    #     periodSeconds = 30
-    #     }
-    #   }
-    # }
-    # volumes {
-    #   name = "config"
-    #   secret{
-    #     secret = google_secret_manager_secret.secret.secret_id
-    #     items {
-    #         key = "latest"
-    #         path = "config.yaml"
-    #     }
-    #   }
-    # }
+    containers {
+      name = "collector"
+      image = "us-docker.pkg.dev/cloud-ops-agents-artifacts/google-cloud-opentelemetry-collector/otelcol-google:0.126.0"
+      args = ["--config=/etc/otelcol-google/config.yaml"]
+      startup_probe {
+        http_get {
+          port = 13133
+          path = "/"
+        }
+      }
+      volume_mounts {
+        mount_path = "/etc/otelcol-google"
+        name = "config_bucket"
+      }
+    }
+    volumes {
+      name = "config_bucket"
+      gcs {
+        bucket = google_storage_bucket.default.name
+        read_only = true
+      }
+    }
   }
+}
+
+resource "random_id" "bucket" {
+  byte_length = 8
+}
+
+resource "google_storage_bucket" "default" {
+  name = "my-bucket-${random_id.bucket.hex}"
+  location = "US"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket_object" "default" {
+ name         = "config.yaml"
+ source       = "config.yaml"
+ content_type = "text/plain"
+ bucket       = google_storage_bucket.default.id
 }
